c81c26867f6a0ec3d05e2f9b60060b1f548ce137
William Moon <william-moon@lambdastudents.com>
2021-07-06 10:59:43 -0700

    get users

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,43 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+     const posts = await Post.find()
+     if (!posts) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        });
+     } else {
+        res.status(200).json(posts);
+     }
+});
+
+router.get('/:id', async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(post)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            error: err.message
+        })
+    }
+})
+
+router.post('/', (req,res) => {
+    
+})
+
+
+
+
+module.exports = router
